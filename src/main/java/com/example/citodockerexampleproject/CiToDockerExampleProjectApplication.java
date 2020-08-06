e9b4f3b16c0bf79f40b1949a533d75f5cb66fb90
Maria Mihai <maria.ic.mihai@gmail.com>
2020-08-06 11:21:07 +0200

    Initial commit. (Simple Spring Boot project.)

---
+++ src/main/java/com/example/citodockerexampleproject/CiToDockerExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.citodockerexampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiToDockerExampleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiToDockerExampleProjectApplication.class, args);
+    }
+
+}
